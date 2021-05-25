50245c7e26fe86ee605db95b4aedb0a885b9d1ea
Armin Vieweg <armin@v.ieweg.de>
2021-05-25 18:19:36 +0200

    [DEVOPS] Add phpunit code coverage
    To get code coverage results in functional tests, I've replaced
    the symfony/process call with the CommandTester.
    
    Also, restructured phpunit configs.

--- tests/Functional/EditorConfig/CommandTest.php
+++ tests/Functional/EditorConfig/CommandTest.php
@@ -1,7 +1,9 @@
 <?php declare(strict_types = 1);
 namespace Armin\EditorconfigCli\Tests\Functional\EditorConfig;
 
+use Armin\EditorconfigCli\Application;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Tester\CommandTester;
 use Symfony\Component\Process\Process;
 
 class CommandTest extends TestCase
@@ -9,69 +11,82 @@ class CommandTest extends TestCase
 
     public function testHelp()
     {
-        $process = new Process([PHP_BINARY, 'bin/ec', '--help']);
-        $process->run();
+        $command = new Application();
+        $command->setAutoExit(false);
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(['--help' => '']);
 
-        $output = $process->getOutput();
-
-        self::assertSame(0, $process->getExitCode());
-        self::assertStringContainsString('bin/ec [options] [--] [<names>...]', $output);
-        self::assertStringContainsString('-n, --no-interaction', $output);
+        self::assertSame(0, $commandTester->getStatusCode());
+        self::assertStringContainsString('CLI tool to validate and auto-fix text files, based on given .editorconfig declarations.', $commandTester->getDisplay());
+        self::assertStringContainsString('[options] [--] [<names>...]', $commandTester->getDisplay());
+        self::assertStringContainsString('-n, --no-interaction', $commandTester->getDisplay());
     }
 
     public function testValidCase()
     {
-        $process = new Process([PHP_BINARY, 'bin/ec', '-d', 'tests/Functional/EditorConfig/Data/valid/']);
-        $process->run();
+        $command = new Application();
+        $command->setAutoExit(false);
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(['-d' => 'tests/Functional/EditorConfig/Data/valid/', '-v' => '', '-s' => ['charset']]);
 
-        self::assertSame(0, $process->getExitCode());
-        self::assertStringContainsString('Done. No issues found.', $process->getOutput());
+        self::assertSame(0, $commandTester->getStatusCode());
+        self::assertStringContainsString('Done. No issues found.', $commandTester->getDisplay());
     }
 
     public function testInvalidCase()
     {
-        $process = new Process([PHP_BINARY, 'bin/ec', '-d', 'tests/Functional/EditorConfig/Data/invalid/']);
-        $process->run();
+        $command = new Application();
+        $command->setAutoExit(false);
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(['-d' => 'tests/Functional/EditorConfig/Data/invalid/']);
 
-        self::assertSame(2, $process->getExitCode());
-        self::assertStringContainsString('/invalid.txt [5]', $process->getOutput());
-        self::assertStringContainsString('This file has line ending "lf" given, but "crlf" is expected', $process->getOutput());
-        self::assertStringContainsString('This file has invalid encoding given! Expected: "latin1", Given: "utf-8"', $process->getOutput());
-        self::assertStringContainsString('This file has trailing whitespaces', $process->getOutput());
-        self::assertStringContainsString('Line 1: Expected indention style "space" but found "tabs"', $process->getOutput());
-        self::assertStringContainsString('Line 5: Max line length (80 chars) exceeded by 123 chars', $process->getOutput());
-        self::assertStringContainsString('This file has no final new line given', $process->getOutput());
+        self::assertSame(2, $commandTester->getStatusCode());
+        self::assertStringContainsString('/invalid.txt [5]', $commandTester->getDisplay());
+        self::assertStringContainsString('This file has line ending "lf" given, but "crlf" is expected', $commandTester->getDisplay());
+        self::assertStringContainsString('This file has invalid encoding given! Expected: "latin1", Given: "utf-8"', $commandTester->getDisplay());
+        self::assertStringContainsString('This file has trailing whitespaces', $commandTester->getDisplay());
+        self::assertStringContainsString('Line 1: Expected indention style "space" but found "tabs"', $commandTester->getDisplay());
+        self::assertStringContainsString('Line 5: Max line length (80 chars) exceeded by 123 chars', $commandTester->getDisplay());
+        self::assertStringContainsString('This file has no final new line given', $commandTester->getDisplay());
     }
 
     public function testTrailingWhitespacesVsAddNewLine()
     {
-        $process = new Process([PHP_BINARY, 'bin/ec', '-d', 'tests/Functional/EditorConfig/Data/trailing-whitespace-vs-add-new-line/']);
-        $process->run();
+        $command = new Application();
+        $command->setAutoExit(false);
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(['-d' => 'tests/Functional/EditorConfig/Data/trailing-whitespace-vs-add-new-line/']);
 
-        self::assertSame(0, $process->getExitCode(), $process->getOutput());
+        self::assertSame(0, $commandTester->getStatusCode(), $commandTester->getDisplay());
     }
 
     public function testMissingFinalLineTriggersTrailingWhitespace()
     {
-        $process = new Process([PHP_BINARY, 'bin/ec', '-d', 'tests/Functional/EditorConfig/Data/missing-final-line-triggers-trailing-whitespace/']);
-        $process->run();
+        $command = new Application();
+        $command->setAutoExit(false);
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(['-d' => 'tests/Functional/EditorConfig/Data/missing-final-line-triggers-trailing-whitespace/']);
 
-        self::assertSame(2, $process->getExitCode());
-        self::assertStringContainsString('Found 1 issue in 1 file', $process->getOutput());
+        self::assertSame(2, $commandTester->getStatusCode());
+        self::assertStringContainsString('Found 1 issue in 1 file', $commandTester->getDisplay());
     }
 
     public function testSkippingRules()
     {
-        $process = new Process([PHP_BINARY, 'bin/ec', '-d', 'tests/Functional/EditorConfig/Data/skip-rules/']);
-        $process->run();
+        $command = new Application();
+        $command->setAutoExit(false);
 
-        self::assertSame(2, $process->getExitCode());
-        self::assertStringContainsString('This file has trailing whitespaces', $process->getOutput());
+        // Test without flag
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(['-d' => 'tests/Functional/EditorConfig/Data/skip-rules/']);
+        self::assertSame(2, $commandTester->getStatusCode());
+        self::assertStringContainsString('This file has trailing whitespaces', $commandTester->getDisplay());
 
-        $process = new Process([PHP_BINARY, 'bin/ec', '-d', 'tests/Functional/EditorConfig/Data/skip-rules/', '-s', 'trim']);
-        $process->run();
+        // Test with flag
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(['-d' => 'tests/Functional/EditorConfig/Data/skip-rules/', '-s' => ['trim']]);
 
-        self::assertStringContainsString('Skipping rules: trim_trailing_whitespace', $process->getOutput());
-        self::assertSame(0, $process->getExitCode());
+        self::assertStringContainsString('Skipping rules: trim_trailing_whitespace', $commandTester->getDisplay());
+        self::assertSame(0, $commandTester->getStatusCode());
     }
 }
